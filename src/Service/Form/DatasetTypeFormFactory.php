4b1f449162d9dbbd73e1acdbab51befc25c16df4
Jim Safley <jimsafley@gmail.com>
2022-10-28 19:47:43 -0400

    Pass correct type

--- src/Service/Form/DatasetTypeFormFactory.php
+++ src/Service/Form/DatasetTypeFormFactory.php
@@ -9,7 +9,7 @@ class DatasetTypeFormFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
-        $form = new DatasetTypeForm(null, $options);
+        $form = new DatasetTypeForm(null, $options ?? []);
         $form->setDatasetTypeManager($services->get('Datavis\DatasetTypeManager'));
         return $form;
     }
